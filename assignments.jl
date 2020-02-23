e60182241f893e68f1b878b903ce7b3d958f03b1
Thorvald Ballestad <thorvalb@stud.ntnu.no>
2020-02-23 16:57:41 +0100

    Updates data output and improves figures

--- assignments.jl
+++ assignments.jl
@@ -447,12 +447,31 @@ inner_list, mat = create_eigenmatrix(grid, number_inside)
 values, vectors = solve_eigenproblem(mat)
 @verbose(values, vectors)
 
+@printf "Values calculated for level %i with grid constant %i\n" LEVEL GRID_CONSTANT
+println("Mode#&\t omega/v\t")
+for (i, value) in enumerate(values)
+    @printf "%i&\t%.8E\n" i value
+end
+println("----")
+println("omega/v\t deltaN")
+for (i, value) in enumerate(values)
+    interesting = i-(1/(4*pi))*value
+    @printf "%f\t%6.8f\n" sqrt(value) interesting
+end
+
 ############## Plotting ####################
+if PLOT==false
+    exit()
+end
 @assert PLOT "Plotting not true"
 @verbose("Begin plotting")
-x,y = get_component_lists(fractal)
-x .-= minimum(x)  # Move to fit with grid
-y .-= minimum(y)
+
+x_list, y_list = get_component_lists(fractal)
+x_list .-= minimum(x_list)  # Move to fit with grid
+y_list .-= minimum(y_list)
+y_list = maximum(y_list) .- y_list  # Flip, because that's how colormesh plots
+x_list /= maximum(x_list)  # Normalize
+y_list /= maximum(y_list)
 
 x = Array{Int}(undef, length(inner_list))
 y = Array{Int}(undef, length(inner_list))
@@ -465,12 +484,21 @@ for mode in 1:NUM_MODES
     plot_grid = plottable_grid(size(grid), inner_list, real.(vectors[:, mode]))
 
     # Colormesh
-    plt.figure(figsize=(10,10), dpi=200)
+#    plt.figure(figsize=(10,10), dpi=200)
     plt.title(@sprintf("Level l=%i, mode number %i\n \$\\omega/v\$ = %.3f", LEVEL, mode, sqrt(values[mode])))
-    plt.pcolormesh(plot_grid)
-    plt.savefig(@sprintf("%smode_%i.png", FIG_DIR, mode))
+    plot_grid = plot_grid[1:10:end, 1:10:end]
+    x = range(0, 1, length=size(plot_grid)[1])
+    y = x
+    plt.pcolormesh(x,y,plot_grid)
+    plt.plot(x_list, y_list, "-", linewidth=0.2, color="black")
+    plt.plot()
+    plt.xlabel("\$x/L\$")
+    plt.ylabel("\$y/L\$")
+    plt.gca().set_aspect("equal")
+    plt.savefig(@sprintf("%smode_%i.pdf", FIG_DIR, mode), dpi=10)
     plt.clf()  # Clear figure
 
+    """
     # Surface
     cmap = plt.cm.coolwarm
     my_map = x-> ifelse(x==255, (0,0,0,0), cmap(x))
@@ -484,6 +512,7 @@ for mode in 1:NUM_MODES
     end
     surf(plot_grid, cmap="coolwarm") #, vmin=vmin, vmax=vmax)
     # #scatter3D(x,y,vectors[:, mode], cmap="coolwarm")
-    plt.savefig(@sprintf("%s/surface/mode_%s.png", FIG_DIR, string(mode)))
+    plt.savefig(@sprintf("%s/surface/mode_%s.pdf", FIG_DIR, string(mode)))
+    """
 end
 @verbose("Done!")

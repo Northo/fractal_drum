30f6dbb9bd1cdadedc7407376d8cc479e862dc3a
Thorvald Ballestad <thorvalb@stud.ntnu.no>
2020-03-03 20:19:52 +0100

    Lazy commit, any things

--- plot_fractal.jl
+++ plot_fractal.jl
@@ -1,7 +1,8 @@
+PLOT = true
 include("utils.jl")
 
 # Constants and setup
-LEVEL = 4
+LEVEL = 2
 GRID_CONSTANT = 1
 FIG_DIR = "modes/"
 NUM_MODES = 10
@@ -15,4 +16,5 @@ x, y = get_component_lists(fractal)
 
 plt.title(@sprintf "Fracal level %i" LEVEL)
 plt.plot(x, y)
+plt.gca().set_aspect("equal")
 plt.savefig(@sprintf "media/fractal_l%i.pdf" LEVEL)
